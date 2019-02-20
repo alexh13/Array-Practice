7e05ac817328e0cb00f471ea0b275fc877e159a0
Alex Hughes <hughes.13.alex@gmail.com>
2019-02-20 15:22:06 -0800

    First commit

---
+++ cmake-build-debug/CMakeFiles/array_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/array_practice.dir/main.cpp.o"
+  "array_practice.pdb"
+  "array_practice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/array_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//------------------------------------------ ARRAY PRACTICE ----------------------------------------------------------
+
+// accept an array of integers and a size argument. It should return true if none of the values are negative.
+// If any of the values are negative it should return false
+bool noNegatives(const int array[], int size) {
+
+
+}
+
+
+// accept an array of integers and a size argument. It should replace any negative values with
+// the corresponding positive value.
+void absoluteValues(int array[], int size) {
+
+
+}
+
+
+// accept an array of chars and a size argument. It should return the number of times that the
+// character 'e' shows up in the array.
+int eCount(const char array[], int size) {
+
+
+}
+
+
+// should be similar to eCount, but instead of counting 'e's it should accept a third argument, a target char.
+// The function should return the number of times the target char shows up in the array.
+int charCount(const char array[], int size, char targetChar) {
+
+
+}
+
+
+// accept an array of integers and a size argument. It should return true if the values are sorted in ascending order.
+// False if they are not.
+bool isSorted(const int array[], int size) {
+
+
+}
+
+
+// accept an array of chars and a size argument. It should return true if there are two adjacent elements in the array
+// with equal values. If there are not, it should return false.
+bool equalNeighbors(const char array[], int size) {
+
+
+}
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
